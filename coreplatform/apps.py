45c860d516ec545d41402ab2e838097b1b4ed423
Orchyl Salmasan <salmasan.orchyl@gmail.com>
2020-08-24 20:05:17 +0800

    Added core platform functionality/Models

---
+++ coreplatform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreplatformConfig(AppConfig):
+    name = 'coreplatform'
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    'coreplatform',
+
 ]
 
 MIDDLEWARE = [
